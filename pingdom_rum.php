bf433c463029f4a21ca2a39017a40c917390c3d8
Craig Phillips <craig@craigphillips.biz>
2017-04-12 10:42:32 +1000

    Initial Commit
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

---
+++ pingdom_rum.php
@@ -0,0 +1,41 @@
+<?php
+namespace Grav\Plugin;
+use Grav\Common\Plugin;
+
+class Pingdom_RUMPlugin extends Plugin
+{
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onAssetsInitialized' => ['onAssetsInitialized', 0]
+        ];
+    }
+    /**
+     * Add GoSquared JS in assets initialisation
+     */
+    public function onAssetsInitialized()
+    {
+        /**
+         * Ok, we're ready to go
+         */
+        $prumid = trim($this->config->get('plugins.pingdom_rum.prumid'));
+        if ($prumid) {
+            $gsjs = <<<GoSquaredJS
+var _prum = [['id', '5519be67abe53d310131abbc'],
+            ['mark', 'firstbyte', (new Date()).getTime()]];
+        (function() {
+            var s = document.getElementsByTagName('script')[0]
+                , p = document.createElement('script');
+            p.async = 'async';
+            p.src = '//rum-static.pingdom.net/prum.min.js';
+            s.parentNode.insertBefore(p, s);
+        })();
+GoSquaredJS;
+
+            $this->grav['assets']->addInlineJs($gsjs);
+        }
+    }
+}

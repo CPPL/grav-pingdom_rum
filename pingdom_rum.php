59d7efc9501867511e6e15bc04a93c2eddee9e8e
Craig Phillips <craig@craigphillips.biz>
2017-06-18 21:12:06 +1000

    Add support for page tags -> RUM tags

--- pingdom_rum.php
+++ pingdom_rum.php
@@ -1,6 +1,9 @@
 <?php
 namespace Grav\Plugin;
 use Grav\Common\Plugin;
+use Grav\Common\Page\Page;
+use RocketTheme\Toolbox\Event\Event;
+
 
 class Pingdom_RUMPlugin extends Plugin
 {
@@ -18,14 +21,40 @@ class Pingdom_RUMPlugin extends Plugin
      */
     public function onAssetsInitialized()
     {
+        /** Don't proceed if we are in the admin plugin */
+        if ($this->isAdmin()) return;
+
+        /**
+         * Make sure we're enabled
+         */
+        if ($this->config->get('plugins.pingdom_rum.prumid') == false) return;
+
         /**
          * Ok, we're ready to go
          */
+
+        $this->enable([
+            'onPageProcessed'           => ['onPageProcessed', 0],
+        ]);
+    }
+
+    public function onPageProcessed(Event $e) {
+        /** @var Page $page */
+        $page = $e['page'];
+        $tagsElement = '';
+
+        if ($this->config->get('plugins.pingdom_rum.autotags') == true && isset($page->taxonomy()['tag'])) {
+            $tags = implode("', '",$page->taxonomy()['tag']);
+            $tagsElement =  ", ['tags', ['$tags']],";
+        }
+
         $prumid = trim($this->config->get('plugins.pingdom_rum.prumid'));
         if ($prumid) {
+
+            /** Assemble our Javascript */
             $gsjs = <<<PingdomRUMJS
 var _prum = [['id', '$prumid'],
-            ['mark', 'firstbyte', (new Date()).getTime()]];
+            ['mark', 'firstbyte', (new Date()).getTime()]$tagsElement];
         (function() {
             var s = document.getElementsByTagName('script')[0]
                 , p = document.createElement('script');
